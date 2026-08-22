e62016d216402a2d7ca3f7f78e4b63d6f9f78269
GOren98 <159132311+GOren98@users.noreply.github.com>
2026-08-22 13:25:40 +0900

    feat: register 6-2 workstation block entities

--- src/main/java/dev/goren98/brewerydelight/registry/ModBlockEntities.java
+++ src/main/java/dev/goren98/brewerydelight/registry/ModBlockEntities.java
@@ -1,6 +1,8 @@
 package dev.goren98.brewerydelight.registry;
 
 import dev.goren98.brewerydelight.BreweryDelight;
+import dev.goren98.brewerydelight.aroma.table.AromaTableBlockEntity;
+import dev.goren98.brewerydelight.brewing.BrewingStationBlockEntity;
 import dev.goren98.brewerydelight.cooking.CookingPotBlockEntity;
 import dev.goren98.brewerydelight.crop.AromaCropBlockEntity;
 import net.minecraft.core.registries.Registries;
@@ -14,5 +16,7 @@ public final class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, BreweryDelight.MOD_ID);
     public static final Supplier<BlockEntityType<AromaCropBlockEntity>> AROMA_CROP = BLOCK_ENTITIES.register("aroma_crop", () -> BlockEntityType.Builder.of(AromaCropBlockEntity::new, ModBlocks.AROMA_BLOCKS.stream().map(Supplier::get).toArray(Block[]::new)).build(null));
     public static final Supplier<BlockEntityType<CookingPotBlockEntity>> COOKING_POT = BLOCK_ENTITIES.register("cooking_pot", () -> BlockEntityType.Builder.of(CookingPotBlockEntity::new, ModBlocks.COOKING_POT.get()).build(null));
+    public static final Supplier<BlockEntityType<BrewingStationBlockEntity>> BREWING_STATION = BLOCK_ENTITIES.register("brewing_station", () -> BlockEntityType.Builder.of(BrewingStationBlockEntity::new, ModBlocks.BREWING_STATION.get()).build(null));
+    public static final Supplier<BlockEntityType<AromaTableBlockEntity>> AROMA_TABLE = BLOCK_ENTITIES.register("aroma_table", () -> BlockEntityType.Builder.of(AromaTableBlockEntity::new, ModBlocks.AROMA_TABLE.get()).build(null));
     private ModBlockEntities() {}
 }
